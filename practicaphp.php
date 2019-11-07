c6a17b9fe925555d73046c7823eeb33d082a7b30
Nico Manzanel <nicomanzanel@hotmail.com>
2019-11-07 17:02:04 -0300

    Clase de 07/11

--- practicaphp.php
+++ practicaphp.php
@@ -165,5 +165,211 @@ if ($cantidadDeAlumnos) {
        /*  10. Agregar al switch anterior un mensaje en caso de que la nota no sea un número del 1 al 10 
         diciendo “El número no es válido”. Punto extra: ¿Probaste con números negativos? */
 
+        function bucles($numero) {
+            $resultado = [];
+            $i=0;
+            while($numero<101){
+              $resultado[$i]=$numero;
+              $numero++;
+            $i++;
+            }
+            
+            return $resultado;
+            echo "<pre>";
+            var_dump($resultado);
+          }
+
+/* ACA COMIENZA LA PRACTICA DEL DIA 07/11/2019
+                BUCLES*/
+
+/* 1  Mostrar la tabla de multiplicar del 2 utilizando un for. */
+for ($i=0; $i < 11; $i++)  { 
+$resultado=0;
+$resultado= 2 * $i;
+echo "2 * "  . "$i" . " = " .$resultado;
+echo "<br>"; 
+}
+echo "<br>"; 
+
+/* Un bucle while que cuente desde 100 hasta 85 (fíjate que en este caso es decreciente). */
+
+
+    $i=100;
+    while($i>=85){
+        echo $i  ;
+        echo "<br>"; 
+        $i--;
+    }   
+
+    echo "<br>"; echo "<br>"; 
+/* Un bucle while que a partir de una variable $contador que toma valores de 1 a 5, muestre por pantalla el doble del valor de $contador, es decir, que muestre 2, 4, 6, 8, 10. */
+for ($i=1; $i < 6; $i++)  { 
+    $resultado=0;
+    $resultado= 2 * $i;
+    echo $resultado;
+
+    echo "<br>"; 
+    }
+    echo "<br>"; echo "<br>"; 
+    /* Utilizando un while haremos un programa que tire una moneda (seleccionará un número al azar que puede ser 0 o 1) hasta que saque 5 veces cara (el número 1). Al terminar, debe imprimir cuántos tiros de monedas llevó obtener 5 veces cara. */
+    $contador=0;
+    $caras=0;
+    while($caras<5){
+        $dado = rand(0,1);
+        echo $dado;
+        echo " - ";
+        if ($dado == 1){
+            $caras++;
+        };
+        $contador++;
+    } 
+    echo "<br>";
+     echo "La cantidad de veces que si tiro el dado para sacar 5 caras es de : " .$contador; echo "<br>";
+    echo "<br>"; echo "<br>";
+   
+    /* Definir un array con 5 strings que sean nombres. Recorrer este array para imprimir cada uno de los nombres en pantalla.
+    Resolver este problema con un for.
+    Resolver este problema con while.
+    Resolver este problema con un do/while.
+    Resolver este problema con un foreach. */
+
+    $nombres=["Nicolas" , "Lisandro" , "Antonela" , "Santiago" , "Lucas" ];
+    $hasta=count($nombres);
+    for ($i=0; $i < $hasta ; $i++) { 
+        echo $nombres[$i];
+        echo "<br>";
+    }
+    echo "<br>";
+    $nombres=["Nicolas" , "Lisandro" , "Antonela" , "Santiago" , "Lucas" ];
+    $i=0;
+    $hasta=count($nombres);
+    while ($i < $hasta) {
+        echo $nombres[$i];
+        echo "<br>";
+        $i++;
+    }
+
+    echo "<br>";
+    $nombres=["Nicolas" , "Lisandro" , "Antonela" , "Santiago" , "Lucas" ];
+    $i=0;
+    $hasta=count($nombres);
+    do {
+       echo $nombres[$i];
+        echo "<br>";
+        $i++; 
+    } while ($i < $hasta);
         
-?>  
+    echo "<br>";echo "<br>";
+    $nombres=["Nicolas" , "Lisandro" , "Antonela" , "Santiago" , "Lucas" ];
+    foreach($nombres as $valor){
+        echo $valor;
+        echo "<br>";
+        
+    }
+    
+/* Definir un array con 10 números aleatorios entre 0 y 10. Recorrer este array para imprimir todos los números. La ejecución debe terminarse si alguno de los números encontrados es un 5 (Se debe imprimir “Se encontró un 5!”) */
+echo "<br>";  echo "<br>";
+ for ($i=0; $i  < 10; $i++){ 
+ $numeros[]=rand(0,10);
+ if($numeros[$i] != 5) {
+    echo $numeros[$i];  
+ echo "<br>";
+ }
+ else{
+     echo "Se encontro un 5!";
+     break;
+ }
+}
+
+/* Definir un array con 10 números aleatorios entre 0 y 100. Recorrer este array contando cuantos números son pares */ 
+echo "<br>";  echo "<br>";
+$pares=0;
+for ($i=0; $i  < 9; $i++){ 
+$numeros[]=rand(0,100);
+echo $numeros[$i];
+echo "<br>";
+if($numeros[$i]%2==0){
+   $pares++;  
+}
+
+}
+echo "<br>";
+echo "Se encontraron " .$pares . " numeros pares";
+
+/* Definir una variable $mascota que sea un array asociativo
+En el índice animal debe decir qué animal es.
+En el índice edad debe decir la edad.
+En el índice altura debe decir la altura.
+En el índice nombre debe decir el nombre
+
+Recorrer los valores del array con un foreach imprimiendo (como ejemplo):
+animal: perro
+edad: 5
+altura: 0,60
+nombre: Sonic */
+echo "<br>";echo "<br>";
+$mascota=[
+    "animal" => "perro",
+    "edad" => 5,
+    "altura" => 0.60,
+    "nombre" => "Sonic"
+];
+foreach($mascota as $clave => $valor){
+    echo $clave .":" .$valor;
+    echo "<br>";
+}
+echo "<br>";echo "<br>";
+
+/* Partiendo de un archivo con la siguiente variable definida:
+Crear un script que muestre el nombre de la capital y el país desde la variable $ceu. */
+$ceu = array( "Italia"=>"Roma",
+ "Luxembourg"=>"Luxembourg", 
+ "Bélgica"=> "Bruselas",
+  "Dinamarca"=>"Copenhagen", 
+  "Finlandia"=>"Helsinki", 
+  "Francia" => "Paris",
+   "Slovakia"=>"Bratislava", 
+   "Eslovenia"=>"Ljubljana",
+    "Alemania" => "Berlin",
+     "Grecia" => "Athenas",
+      "Irlanda"=>"Dublin", "Holanda"=>"Amsterdam", "Portugal"=>"Lisbon", "España"=>"Madrid", "Suecia"=>"Stockholm", "Reino Unido"=>"London", "Chipre"=>"Nicosia", "Lithuania"=>"Vilnius", "Republica Checa"=>"Prague", "Estonia"=>"Tallin", "Hungría"=>"Budapest", "Latvia"=>"Riga", "Malta"=>"Valletta", "Austria" => "Vienna", "Polonia"=>"Warsaw") ;
+
+      foreach($ceu as $pais => $capital){
+        echo $capital ." " .$pais . ";";
+        echo "<br>";
+      }
+      echo "<br>";echo "<br>";
+      
+      $ceu = [
+      
+              "Argentina"        => ["Buenos Aires", "Córdoba", "Santa Fé"],
+      
+              "Brasil" => ["Brasilia", "Rio de Janeiro", "Sao Pablo"],
+      
+              "Colombia" => ["Cartagena", "Bogota", "Barranquilla"],
+      
+              "Francia" => ["Paris", "Nantes", "Lyon"],
+      
+              "Italia" => ["Roma", "Milan", "Venecia"],
+      
+              "Alemania" => ["Munich", "Berlin", "Frankfurt"]
+      
+      ];
+      foreach($ceu as $pais => $capital){
+        echo "<br>";
+      }
+      
+      /* Crear un script que muestre el nombre de cada país y sus ciudades desde la variable $ceu con el siguiente formato:
+      
+      
+              Las ciudades de Argentina son:
+      
+      Buenos Aires
+      Córdoba
+      Santa Fé
+              Las ciudades de Brasil son:
+      
+      Brasilia
+      Rio de Janeiro
+      Sao Pablo */
+?>
